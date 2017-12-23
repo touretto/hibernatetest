01cc27775b6fbf11fbbbcc3d2563e70a25ca4228
Jens Kager <mail@j-k.at>
2017-12-23 14:00:15 +0100

    Create Person

--- src/main/java/Hibernator.java
+++ src/main/java/Hibernator.java
@@ -1,18 +1,38 @@
 import Models.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class Hibernator {
-    private SessionFactory sessionFactory;
+    private SessionFactory sessionFactory = null;
 
-    public void initHibernate() {
-        sessionFactory = new Configuration().configure().buildSessionFactory();
+    private void initialize() {
+        Configuration config = new Configuration().configure();
+        config.setProperty("hibernate.temp.use_jdbc_metadata_defaults","false");
+        sessionFactory = config.buildSessionFactory();
+    }
+
+    private Session getSession() {
+        if (sessionFactory == null)
+            initialize();
+
+        return sessionFactory.openSession();
     }
 
     public Person getPersonById(int id) {
-        Session session = sessionFactory.openSession();
+        Session session = getSession();
 
         return session.get(Person.class, id);
+    }
+
+    public int createPerson(Person person) {
+        Session session = getSession();
+
+        Transaction transaction = session.beginTransaction();
+        session.save(person);
+        transaction.commit();
+
+        return person.getId();
     }
 }
--- src/test/java/HibernatorTest.java
+++ src/test/java/HibernatorTest.java
@@ -1,30 +1,47 @@
 import Models.Person;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HibernatorTest {
-    private Hibernator hibernator;
+    private static Hibernator hibernator;
 
-    @BeforeEach
-    void SetUp() {
+    @BeforeAll
+    static void SetUp() {
         hibernator = new Hibernator();
-    }
-
-    @Test
-    void initHibernate_doesNotThrow() {
-        hibernator.initHibernate();
     }
 
     @Test
     void getPersonById_withId1_returnsObject() {
         final int id = 1;
 
-        hibernator.initHibernate();
         Person person = hibernator.getPersonById(id);
         assertEquals(id, person.getId());
 
         System.out.println("Person " + person.getId() + ": " + person.getName());
+    }
+
+    @Test
+    void createPerson_returnsNewId() {
+        Person newPerson = new Person();
+        newPerson.setName("Test");
+
+        int newId = hibernator.createPerson(newPerson);
+        assertNotEquals(0, newId);
+        System.out.println("Create person ID: " + newId);
+    }
+
+    @Test
+    void getPersonById_withIdFromCreatePerson_returnsCreatedPerson() {
+        Person newPerson = new Person();
+        newPerson.setName("Test");
+
+        int newId = hibernator.createPerson(newPerson);
+        assertNotEquals(0, newId);
+
+        Person createdPerson = hibernator.getPersonById(newId);
+        assertNotNull(createdPerson);
+        assertEquals(newId, createdPerson.getId());
     }
 }
